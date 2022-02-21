0d3cc4c16f7a845256fa70150916d25dc08d5df3
Algoriithm <algoriithm@uiyuito.local>
2022-02-21 21:49:25 +0545

    balance save

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -5,7 +5,7 @@
 // export async function handleEvent(event: SubstrateEvent): Promise<void> {
 //     const {event: {data: [account, balance]}} = event;
 //     //Create a new Account entity with ID using block hash
-// 	let record = new
+// 	   let record = new
 //     Account(event.extrinsic.block.block.header.hash.toString());
 //     //Assign the Polkadot address to the account field
 //     record.account = account.toString();
@@ -17,7 +17,7 @@
 import {SubstrateEvent, SubstrateBlock} from "@subql/types";
 import {Account} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
-import {EventRecord } from "@polkadot/types/interfaces/system";
+import { EventRecord } from "@polkadot/types/interfaces/system";
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {    
     let blockNumber = block.block.header.number.toBigInt();
@@ -56,15 +56,6 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
             case "Withdraw":
                 handleWithdraw(block, eventRecord); 
                 break;
-            case "ExtrinsicSuccess":
-                handleExtrinsicSuccess(block, eventRecord);
-                break;
-            case "CandidateIncluded":
-                handleCandidateIncluded(block, eventRecord);
-                break;
-            case "CandidateBacked":
-                handleCandidateBacked(block, eventRecord);
-                break;
             default:
                 logger.info("Ignoring method -- "+ method)
                 break;
@@ -77,6 +68,13 @@ export async function handleBalanceSet(block: SubstrateBlock, event: EventRecord
     const [account, balance1, balance2] = event.event.data.toJSON() as [string, bigint, bigint];
     logger.info(`Handling BalanceSet!: ${JSON.stringify(event)}`);
     logger.info("Account " + account + " Balance " + balance1 + " Balance2 " + balance2)
+    
+    //Create a new Account entity with ID using block hash
+    let record = new Account(block.block.header.hash.toString());
+    
+    record.account = account.toString();
+    record.balance = balance1+balance2
+    await record.save();
 }
   
 export async function handleDeposit(block: SubstrateBlock, event: EventRecord): Promise<void> { 
@@ -115,6 +113,7 @@ export async function handleTransfer(block: SubstrateBlock, event: EventRecord):
     logger.info(`Handling Transfer!: ${JSON.stringify(event)}`);
     logger.info("Account " + account + " Account2 " + account2 + " Balance " + balance) 
 }
+
 export async function handleUnreserved(block: SubstrateBlock, event: EventRecord): Promise<void> { 
     const [account, balance] = event.event.data.toJSON() as [string, bigint];
     logger.info(`Handling Unreserved!: ${JSON.stringify(event)}`);
@@ -125,22 +124,3 @@ export async function handleWithdraw(block: SubstrateBlock, event: EventRecord):
     logger.info(`Handling Withdraw!: ${JSON.stringify(event)}`);
     logger.info("Account " + account + " Balance " + balance) 
 }
-
-export async function handleExtrinsicSuccess(block: SubstrateBlock, event: EventRecord): Promise<void> {
-    const [account, balance] = event.event.data.toJSON() as [string, bigint];
-    logger.info(`Handling ExtrinsicSuccess!: ${JSON.stringify(event)}`);
-    logger.info("Account " + account + " Balance " + balance) 
-}
-
-export async function handleCandidateIncluded(block: SubstrateBlock, event: EventRecord): Promise<void> {
-    const [account, balance] = event.event.data.toJSON() as [string, bigint];
-    logger.info(`Handling CandidateIncluded!: ${JSON.stringify(event)}`);
-    logger.info("Account " + account + " Balance " + balance) 
-}
-
-export async function handleCandidateBacked(block: SubstrateBlock, event: EventRecord): Promise<void> {
-    const [account, balance] = event.event.data.toJSON() as [string, bigint];
-    logger.info(`Handling CandidateBacked!: ${JSON.stringify(event)}`);
-    logger.info("Account " + account + " Balance " + balance) 
-}
-
